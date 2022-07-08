a017c156d556e12968055cfc7c6cf8535383a266
fugu <thefugupost@gmail.com>
2022-07-08 17:26:10 +0500

    update

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,17 +4,17 @@ import getRandomNumber from '../../utils/helpers/utils.js';
 const discription = 'Find the greatest common divisor of given numbers.';
 
 const isGcd = (a, b) => {
-  let number1 = a;
-  let number2 = b;
+  let num1 = a;
+  let num2 = b;
   let gcd = '';
 
-  while (number1 !== number2) {
-    if (number1 > number2) {
-      number1 -= number2;
+  while (num1 !== num2) {
+    if (num1 > num2) {
+      num1 -= num2;
     } else {
-      number2 -= number1;
+      num2 -= num1;
     }
-    gcd = number1;
+    gcd = num1;
   }
   return gcd;
 };
