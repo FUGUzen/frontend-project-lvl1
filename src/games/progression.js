79f2fb2cec8af61d07c3a75fac34f1f6f5238765
fugu <thefugupost@gmail.com>
2022-07-08 15:08:48 +0500

    update

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,19 +1,19 @@
-import getRandomNumber from '../getRandomNumber.js';
-import gameEngine from '../index.js';
+import runEngine from '../index.js';
+import getRandomNumber from '../../utils/helpers/utils.js';
 
 const discription = 'What number is missing in the progression?';
 
 const lengthProgression = 10;
 
 const brainProgression = () => {
-  const firstNum = getRandomNumber(30);
-  const step = getRandomNumber(5);
+  const firstNum = getRandomNumber(1, 30);
+  const step = getRandomNumber(1, 5);
   const arr = [];
 
   for (let i = 1; i <= lengthProgression; i += 1) {
     arr.push(firstNum + i * step);
   }
-  const hiddenElementPosition = getRandomNumber(lengthProgression);
+  const hiddenElementPosition = getRandomNumber(1, lengthProgression) - 1;
 
   const rightAnswer = String(arr[hiddenElementPosition]);
   arr[hiddenElementPosition] = '..';
@@ -24,7 +24,7 @@ const brainProgression = () => {
 };
 
 const startBrainProgressionGame = () => {
-  gameEngine(discription, brainProgression);
+  runEngine(discription, brainProgression);
 };
 
 export default startBrainProgressionGame;
